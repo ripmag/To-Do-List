a51bd6a4c851f3dc5d0bf6fff7a4024495d83561
Eduard <ripmag@gmail.com>
2022-09-06 10:38:11 +0200

    feat: Routes with 2 pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+    <div className="container">
+      <Routes>
+        <Route path={'/'} element={<Home/>} exact/>
+        <Route path={'/about'} exact element={<About/>}/>
+        
+      </Routes>
+    </div>
+    </BrowserRouter>
   );
 }
 
