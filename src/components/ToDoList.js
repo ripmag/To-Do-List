bd8a954e702ebf9ac3f7d69ff15ae51097cd438d
Eduard <ripmag@gmail.com>
2022-09-06 18:07:45 +0200

    feat: listItem animation

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,22 +1,28 @@
 import React, { useContext } from 'react';
 import { AlertContext } from '../context/alert/alertContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 
 const ToDoList = ({ list, onRemove }) => {
-    const alert=useContext(AlertContext)
-    
-return (       
-    <ul className='list-group '>
-        {list.map(elem => (
-            <li className='list-group-item list' key={elem.id}>
-                <div>
-                    <strong>{elem.title} </strong>
-                    <span>{elem.date}</span>
-                </div>
-                <button type="button" className="btn btn-outline-danger btn-sm" onClick={() => onRemove(elem.id).then(alert.show(`Дело ${elem.title} успешно удалено`))} >delete</button>
-            </li>
-        ))}
-    </ul>
-)}
+    const alert = useContext(AlertContext)
+
+    return (
+        <TransitionGroup component='ul' className='list-group '>
+            {list.map(elem => (
+                <CSSTransition
+                classNames={'list'}
+                timeout={1000}>
+                    <li className='list-group-item list' key={elem.id}>
+                        <div>
+                            <strong>{elem.title} </strong>
+                            <span>{elem.date}</span>
+                        </div>
+                        <button type="button" className="btn btn-outline-danger btn-sm" onClick={() => onRemove(elem.id).then(alert.show(`Дело ${elem.title} успешно удалено`))} >delete</button>
+                    </li>
+                </CSSTransition>
+            ))}
+        </TransitionGroup>
+    )
+}
 
 export default ToDoList;
